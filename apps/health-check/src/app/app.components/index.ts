ff43a0080a633566e9f683fcbb6e41fee21b4544
Tim Pierce <disdainful777@gmail.com>
2024-01-08 22:06:46 -0700

    created priority enum for components

---
+++ apps/health-check/src/app/app.components/index.ts
@@ -0,0 +1,18 @@
+import { BaseComponent } from './base.component'
+import { createDiscordComponent } from './discord.component'
+import { createCloudflareComponent } from './cloudflare.component'
+import { createDiscordBotComponent } from './discord.bot.component'
+import { createMySQLComponent } from './mysql.component'
+
+const initializeComponents = async (): Promise<BaseComponent[]> => {
+  const discordComponent = await createDiscordComponent()
+  const cloudflareComponent = await createCloudflareComponent()
+  const mysqlComponent = await createMySQLComponent()
+
+  const discordBotComponent = await createDiscordBotComponent()
+
+
+  return [discordComponent, cloudflareComponent, mysqlComponent, discordBotComponent]
+}
+
+export default initializeComponents
