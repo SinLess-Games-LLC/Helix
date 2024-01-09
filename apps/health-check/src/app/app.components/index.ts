a33cf64d877a03f93f891cee25d1c4c9d2c10aab
Tim Pierce <disdainful777@gmail.com>
2024-01-08 23:09:04 -0700

    added gateway api health check

--- apps/health-check/src/app/app.components/index.ts
+++ apps/health-check/src/app/app.components/index.ts
@@ -3,15 +3,23 @@ import { createDiscordComponent } from './discord.component'
 import { createCloudflareComponent } from './cloudflare.component'
 import { createDiscordBotComponent } from './discord.bot.component'
 import { createMySQLComponent } from './mysql.component'
+import { createGatewayApiComponent } from './gateway.api.component'
 
 const initializeComponents = async (): Promise<BaseComponent[]> => {
   const discordComponent = await createDiscordComponent()
   const cloudflareComponent = await createCloudflareComponent()
   const mysqlComponent = await createMySQLComponent()
+  const gatewayApiComponent = await createGatewayApiComponent()
 
   const discordBotComponent = await createDiscordBotComponent()
 
-  return [discordComponent, cloudflareComponent, mysqlComponent, discordBotComponent]
+  return [
+    discordComponent,
+    cloudflareComponent,
+    mysqlComponent,
+    discordBotComponent,
+    gatewayApiComponent,
+  ]
 }
 
 export default initializeComponents
