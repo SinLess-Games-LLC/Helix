e4a6dd89685bca61c77903d40008eb3aa9b9db4b
Tim Pierce <disdainful777@gmail.com>
2024-01-08 22:24:44 -0700

    chanded how the constants were named

--- libs/helix-utilities/src/constants.ts
+++ libs/helix-utilities/src/constants.ts
@@ -1,5 +1,6 @@
-export const discordStatusSummaryUrl = 'https://discordstatus.com/api/v2/summary.json'
-export const discordStatusUrl = 'https://discordstatus.com/api/v2/status.json'
-export const cloudflareStatusSummaryUrl = 'https://www.cloudflarestatus.com/api/v2/summary.json'
+export const DiscordStatusSummaryUrl: string = 'https://discordstatus.com/api/v2/summary.json'
+export const DiscordStatusUrl: string = 'https://discordstatus.com/api/v2/status.json'
+export const CloudflareStatusSummaryUrl: string =
+  'https://www.cloudflarestatus.com/api/v2/summary.json'
 
-export const cloudflareStatusUrl = 'https://www.cloudflarestatus.com/api/v2/status.json'
+export const CloudflareStatusUrl: string = 'https://www.cloudflarestatus.com/api/v2/status.json'
