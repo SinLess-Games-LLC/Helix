e9b100ca2a00dde1e1c50258c2e2b806a62df0ad
Tim Pierce <disdainful777@gmail.com>
2024-01-09 00:12:36 -0700

    created redis component and health check

--- apps/health-check/src/app/app.components/index.ts
+++ apps/health-check/src/app/app.components/index.ts
@@ -4,19 +4,22 @@ import { createCloudflareComponent } from './cloudflare.component'
 import { createDiscordBotComponent } from './discord.bot.component'
 import { createMySQLComponent } from './mysql.component'
 import { createGatewayApiComponent } from './gateway.api.component'
+import { createRedisComponent } from './redis.component'
 
 const initializeComponents = async (): Promise<BaseComponent[]> => {
-  const discordComponent = await createDiscordComponent()
-  const cloudflareComponent = await createCloudflareComponent()
-  const mysqlComponent = await createMySQLComponent()
-  const gatewayApiComponent = await createGatewayApiComponent()
+  const discordComponent = await createDiscordComponent(1)
+  const cloudflareComponent = await createCloudflareComponent(2)
+  const mysqlComponent = await createMySQLComponent(3)
+  const redisComponent = await createRedisComponent(4)
 
-  const discordBotComponent = await createDiscordBotComponent()
+  const discordBotComponent = await createDiscordBotComponent(5)
+  const gatewayApiComponent = await createGatewayApiComponent(6)
 
   return [
     discordComponent,
     cloudflareComponent,
     mysqlComponent,
+    redisComponent,
     discordBotComponent,
     gatewayApiComponent,
   ]
